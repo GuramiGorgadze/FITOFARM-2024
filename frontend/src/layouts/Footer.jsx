c4bd8a699d87bb9ae27abea622b34d3875b256e5
GuramiGorgadze <g.gorgadze111@gmail.com>
2026-08-24 12:58:57 +0400

    fixed footer

--- frontend/src/layouts/Footer.jsx
+++ frontend/src/layouts/Footer.jsx
@@ -1,69 +1,86 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Logo from '../assets/logo.png';
 
 function Footer() {
+  const { t } = useTranslation();
+  const year = new Date().getFullYear();
+
   return (
     <footer className="footer">
       <div className="top">
         <div className="column">
-          <h2>Privacy Policy</h2>
-
+          <h2>{t('footer.legalTitle')}</h2>
           <div className="links">
-            <div className="link">Terms of Use</div>
-            <div className="link">Data Privacy</div>
-            <div className="link">Cookie Managment</div>
-            <div className="link">Sitemap</div>
+            <div className="link static">{t('footer.termsOfUse')}</div>
+            <div className="link static">{t('footer.dataPrivacy')}</div>
+            <div className="link static">{t('footer.cookieManagement')}</div>
+            <div className="link static">{t('footer.sitemap')}</div>
           </div>
         </div>
 
         <div className="column">
-          <h2>Follow Us</h2>
-
+          <h2>{t('footer.followTitle')}</h2>
           <div className="links">
-            <a className="link" href='https://www.facebook.com/' target='blank'>Facebook</a>
-            <a className="link" href='https://www.linkedin.com/' target='blank'>Linked In</a>
-            <a className="link" href='https://www.instagram.com/' target='blank'>Instagram</a>
-            <a className="link" href='https://www.youtube.com/' target='blank'>Youtube</a>
+            <a className="link" href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-facebook" aria-hidden="true" />
+              Facebook
+            </a>
+            <a className="link" href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-linkedin" aria-hidden="true" />
+              LinkedIn
+            </a>
+            <a className="link" href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-instagram" aria-hidden="true" />
+              Instagram
+            </a>
+            <a className="link" href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+              <i className="bi bi-youtube" aria-hidden="true" />
+              YouTube
+            </a>
           </div>
         </div>
 
         <div className="column">
-          <h2>Contact Us</h2>
-
+          <h2>{t('footer.contactTitle')}</h2>
           <div className="links">
-            <div className="link">Media</div>
+            <div className="link static">{t('footer.media')}</div>
           </div>
         </div>
 
         <div className="column">
-          <h2>Contact Information</h2>
-
+          <h2>{t('footer.infoTitle')}</h2>
           <div className="links">
-            <div className="link">საქართველო, თბილისი, წერეთლის 142ა.</div>
-            <div className="link">საქართველო, ქალაქი ტყიბული, სოფ. დაბაძველი</div>
-            <div className="link">ტელ.: 599 14 21 18</div>
+            <div className="link static">
+              <i className="bi bi-geo-alt" aria-hidden="true" />
+              {t('info.office1Address')}
+            </div>
+            <div className="link static">
+              <i className="bi bi-geo-alt" aria-hidden="true" />
+              {t('info.office2Address')}
+            </div>
+            <div className="link static">
+              <i className="bi bi-telephone" aria-hidden="true" />
+              {t('info.phone')}
+            </div>
           </div>
         </div>
 
-        <div className="column">
+        <div className="column brand">
           <div className="title">
-            {' '}
-            <img
-              className="logo"
-              src={Logo}
-              alt=""
-            />
-            <div className="title-text">FitoFarm-2024</div>
+            <img className="logo" src={Logo} alt={t('info.logoAlt')} />
+            <p className="wordmark">
+              FITOFARM-<span className="wordmark__year">2024</span>
+            </p>
           </div>
-
-          <p className="slogan">A Natural Path to Well-Being</p>
+          <p className="slogan">{t('footer.slogan')}</p>
         </div>
       </div>
 
-      <div className="divider"></div>
+      <div className="divider" />
 
       <div className="bottom">
-        <p>© 2026 FitoFarm-2024. All rights reserved.</p>
+        <p>{t('footer.copyright', { year })}</p>
       </div>
     </footer>
   );
