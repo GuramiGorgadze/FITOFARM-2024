7f5db5b5e2cb731569f176ffa15df338bc9b57c5
GuramiGorgadze <g.gorgadze111@gmail.com>
2026-08-31 21:22:43 +0400

    removed changes:

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,25 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import PluginCritical from 'rollup-plugin-critical'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    PluginCritical({
-      criticalUrl: './dist/',
-      criticalBase: './dist/',
-      criticalPages: [
-        { uri: 'index.html', template: 'index' },
-      ],
-      criticalConfig: {
-        inline: true,
-        extract: true,
-        width: 1300,
-        height: 900,
-      },
-    }),
-  ],
+  plugins: [react()],
   build: {
     cssMinify: 'esbuild',
   },
